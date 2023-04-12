5884d7544710a3815b4c6f740bb02ff304fd5059
GG <1592860538@qq.com>
2023-04-12 16:13:48 +0800

    user center v1

---
+++ src/main/java/com/example/apilist/ApiListApplication.java
@@ -0,0 +1,13 @@
+package com.example.apilist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apilist/ApiListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apilist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
